3df1e9d858ac6b17a0b24d5a97a06c858eeed916
Neil-Jordan <neilj.kitson@gmail.com>
2022-01-06 17:10:59 -0500

    Removed scrolling while nav is open

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+const body = document.body;
 
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+  document.body.classList.toggle("fixed-position");
+});
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+    document.body.classList.remove("fixed-position");
+  });
+});
